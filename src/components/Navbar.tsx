96b65d92fdb8608fced8267b4dabb345a499807d
Ishara <isharamadushankab@gmail.com>
2024-12-26 23:48:21 +0530

    update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,12 +1,24 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import MenuButton from "./MenuButton"
 
 export default function Navbar() {
     const [showLinks, setShowLinks] = useState(false)
 
-    const isOpen = (val:boolean) => {
+    const isOpen = (val: boolean) => {
         setShowLinks(val);
     }
+
+    useEffect(() => {
+        if (showLinks) {
+            document.body.style.overflow = 'hidden'; // Disable scroll
+        } else {
+            document.body.style.overflow = ''; // Re-enable scroll
+        }
+
+        return () => {
+            document.body.style.overflow = ''; // Cleanup on unmount
+        };
+    }, [showLinks]);
 
     return (
         <>
@@ -18,10 +30,10 @@ export default function Navbar() {
                 </ul>
             </div>
             <div className={`flex md:hidden items-center pr-5 md:pr-0`}>
-                <MenuButton statue={showLinks} isOpen={isOpen}/>
+                <MenuButton statue={showLinks} isOpen={isOpen} />
             </div>
-            <div className={`flex md:hidden ${showLinks ? 'translate-x-0 opacity-100' : 'translate-x-full opacity-0'} duration-500 w-full h-[calc(100vh-56px)] bg-white fixed top-14`}>
-                <ul onClick={()=>{setShowLinks(false)}} className="flex gap-10 text-zinc-500 text-lg flex-col pl-5 pt-10">
+            <div className={`flex md:hidden ${showLinks ? 'translate-x-0 opacity-100 overflow-hidden' : 'translate-x-full opacity-0'} fixed duration-500 w-full h-lvh bg-white top-14 z-10`}>
+                <ul onClick={() => { setShowLinks(false) }} className="flex gap-10 text-zinc-500 text-lg flex-col pl-5 pt-10">
                     <li><a href="/" className={`text-black`}>Home</a></li>
                     <li><a href="#projects">Projects</a></li>
                     <li><a href="#contact">Contact</a></li>
