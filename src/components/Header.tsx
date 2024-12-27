61f4af12cf1226b2263d337774b53123bc0af377
Ishara <isharamadushankab@gmail.com>
2024-12-27 18:58:49 +0530

    update 5 files

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ import homeData from "../data/home.json"
 export default function Header() {
   return (
     <div
-      className={`flex justify-between w-full h-14 items-center text-sm animate-slide-down`}>
+      className={`flex justify-between w-full h-14 items-center text-sm animate-slide-down relative z-50`}>
       <div className={`items-center flex gap-2 pl-2`}>
         <div className={`flex animate-pulse rounded-full w-5 h-5`} title="Available for hire" style={{backgroundColor:homeData.color}}/>
         <a href="/">{homeData.name[0]}</a>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,11 +6,11 @@ export default function Home() {
 
     return (
         <div id="home" className={`flex h-auto md:h-[520px] flex-col-reverse md:flex-row w-full gap-y-5 md:gap-y-0 md:gap-x-5 justify-center md:justify-between pb-2`}>
-            <div className="flex h-auto md:h-full w-full md:w-8/12 rounded-3xl animate-slide-up overflow-hidden justify-center items-center z-[-1]">
-                <div className={`w-full h-full object-cover absolute`}>
-                    <img src={homeData.customGradientImage || placeholder} className={`w-full h-full object-cover animate-halfRotate z-0`} style={{ transformOrigin: "center center", scale: "1.5" }} />
+            <div className="flex h-auto md:h-full w-full md:w-8/12 rounded-3xl animate-slide-up overflow-hidden justify-center items-center">
+                <div className={`w-full h-full object-cover absolute z-[-1]`}>
+                    <img src={homeData.customGradientImage || placeholder} className={`w-full h-full object-cover animate-halfRotate `} style={{ transformOrigin: "center center", scale: "1.5" }} />
                 </div>
-                <div className="flex h-[500px] md:h-full w-full bg-slate-500 bg-opacity-10 p-10 flex-col justify-between z-10">
+                <div className="flex h-[500px] md:h-full w-full bg-slate-500 bg-opacity-10 p-10 flex-col justify-between">
                     <div className="flex flex-col">
                         <h1 className={`text-2xl md:text-5xl`}>{homeData.title}</h1>
                         <p className={`text-base font-normal my-10`}>{homeData.description}</p>
