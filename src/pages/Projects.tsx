91a90197b93eab3c7a5e052b1ca81cd87b97b728
Ishara <isharamadushankab@gmail.com>
2024-12-27 18:19:18 +0530

    update ProjectCard.tsx, projects.json, Home.tsx and Projects.tsx

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -7,7 +7,7 @@ export default function Projects() {
     <div id='projects' className={`grid md:grid-cols-2 justify-items-center gap-5 mb-5 pt-3`}>
       {
         projectData.map((project, index) => (
-          <ProjectCard key={index} id={project.id}/>
+          <ProjectCard key={index} id={projectData.length - index}/>
         ))
       }
     </div>
