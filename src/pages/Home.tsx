b6abb307c9a234b10faf3d954d8fc6b9e88c2494
Ishara <isharamadushankab@gmail.com>
2024-12-27 17:28:06 +0530

    update Navbar.tsx and Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,7 +5,7 @@ import ContactLinks from "../components/ContactLinks";
 export default function Home() {
 
     return (
-        <div id="home" className={`flex h-auto md:h-[520px] flex-col-reverse md:flex-row w-full gap-y-5 md:gap-y-0 md:gap-x-5 justify-center md:justify-between pb-2`}>
+        <div id="home" className={`flex h-auto md:h-[520px] flex-col-reverse md:flex-row w-full gap-y-5 md:gap-y-0 md:gap-x-5 justify-center md:justify-between pb-2 z-[-1]`}>
             <div className="flex h-auto md:h-full w-full md:w-8/12 rounded-3xl animate-slide-up overflow-hidden justify-center items-center ">
                 <div className={`w-full h-full object-cover absolute`}>
                     <img src={homeData.customGradientImage || placeholder} className={`w-full h-full object-cover animate-halfRotate z-0`} style={{ transformOrigin: "center center", scale: "1.5" }} />
