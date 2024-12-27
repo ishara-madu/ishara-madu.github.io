0261c2a89417071c9de2df042f5680cacacb8bcd
Ishara <isharamadushankab@gmail.com>
2024-12-27 13:56:10 +0530

    update ProjectCard.tsx and projects.json

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -6,17 +6,17 @@ interface ProjectCardProps {
 export default function ProjectCard(id: ProjectCardProps) {
     const project = projectsData.find((p) => p.id === id.id)
     return (
-        <div className={`w-full bg-slate-200 h-auto rounded-3xl md:h-[500px] animate-slide-up overflow-hidden relative group`}>
-            <img src={project?.image} alt="" loading='lazy' className={`w-full h-full object-cover z-[-1] absolute group-hover:scale-105 duration-300`} />
-            <div className="flex text-xl font-normal justify-between items-center p-10">
-                <div className="flex flex-col w-full h-full" style={{ color: project?.textColor || "black" }}>
+        <div className={`w-full bg-slate-200 min-h-72 sm:min-h-96 rounded-3xl md:h-[500px] animate-slide-up overflow-hidden relative group`}>
+            <img src={project?.image} alt={project?.title} decoding='async' loading='lazy' className={`w-full h-full object-cover z-[-1] absolute group-hover:scale-105 duration-300`} />
+            <div className="flex justify-between items-center p-5 sm:p-10">
+                <div className="flex flex-col text-base sm:text-xl w-full h-full" style={{ color: project?.textColor || "black" }}>
                     {project?.title}
-                    <div className="flex text-base font-thin">
+                    <div className="flex text-xs sm:text-base font-thin">
                         {project?.description}
                     </div>
                 </div>
-                    <a href={project?.homepage} target='_blank' className="flex bg-white w-12 h-12 rounded-full flex-none justify-center items-center group-hover:animate-bounce cursor-pointer">
-                    <GoArrowUpRight size={24} />
+                    <a href={project?.homepage} target='_blank' className="flex bg-white w-8 h-8 sm:w-12 sm:h-12 rounded-full flex-none justify-center items-center group-hover:animate-bounce cursor-pointer">
+                    <GoArrowUpRight className={`w-4 h-4 sm:w-5 sm:h-5`} />
                     </a>
             </div>
         </div>
