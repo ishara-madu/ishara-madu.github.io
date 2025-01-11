087a847eeca4b188a48f5cb65e6dd55f331561ad
Ishara <isharamadushankab@gmail.com>
2025-01-11 14:12:42 +0530

    update index.html and HeadOptions.tsx

--- src/components/HeadOptions.tsx
+++ src/components/HeadOptions.tsx
@@ -4,17 +4,6 @@ import homeData from '../data/home.json'
 export default function HeadOptions() {
     useEffect(() => {
 
-        //favicon
-
-        const svg = `
-        <svg width="40" height="40" xmlns="http://www.w3.org/2000/svg">
-        <circle cx="20" cy="20" r="20" fill="${homeData.color}" />
-        </svg>
-        `;
-
-        const favicon = document.querySelector('link[rel="icon"]');
-        favicon?.setAttribute('href', 'data:image/svg+xml;utf8,' + encodeURIComponent(svg));
-
         //title
 
         const handleVisibilityChange = () => {
