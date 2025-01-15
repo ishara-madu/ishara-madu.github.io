d10e4c1caa0d61a1da3a31f32f67e0bfe54e8de7
Ishara <isharamadushankab@gmail.com>
2025-01-16 00:15:52 +0530

    update index.html, package.json, Ads.txt and HeadOptions.tsx

--- src/components/HeadOptions.tsx
+++ src/components/HeadOptions.tsx
@@ -4,11 +4,17 @@ import homeData from '../data/home.json'
 export default function HeadOptions() {
     useEffect(() => {
 
+        // Change favicon dynamically
+        const link = document.querySelector("link[rel='icon']") as HTMLLinkElement;
+        if (link) {
+            link.href = "/favicon.png"; // Update the path if needed
+        }
+
         //title
 
         const handleVisibilityChange = () => {
             if (document.hidden) {
-                document.title = 'Hey there';
+                document.title = 'Hey there! 👋';
             } else {
                 document.title = homeData.name[1];
             }
